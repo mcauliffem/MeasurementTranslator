17c1603f9c0cd8f4bb7973b0e3762cfe2ec17ced
Matthew McAuliffe <mcauliffem@icloud.com>
2023-08-22 09:24:47 -0400

    Added typecasting for poll input to make conditional checks pass correctly.

--- measureTranslations/measurement/views.py
+++ measureTranslations/measurement/views.py
@@ -24,20 +24,23 @@ def home(request):
             i_s = ''
             if i != 0:
                 i_s = e.number_to_words(i)
-                i_s = f'{i_s} and '
             
-            n = request.POST['numerator']
+            n = int(request.POST['numerator'])
             n_s = ''
             fraction = ''
             u2 = 'inch'
             if n != 0:
                 n_s = e.number_to_words(n)
                 u2_s = e. plural(u2, n)
-                d = request.POST['denominator']
-                fraction = n_s
+                d = int(request.POST['denominator'])
+                if i != 0:
+                    fraction = f' and {n_s}'
+                else:
+                    fraction = n_s
                 if d != 1:
-                    fraction = convert_denominator(fraction,request.POST['numerator'],d)
-
+                    fraction = convert_denominator(fraction,n,d)
+            else:
+                u2_s = e. plural(u2, i)
             ut_s = f'{f_s}{i_s}{fraction} {u2_s}'
             es_s = GoogleTranslator(source='en', target='es').translate(ut_s)
             uk_s = GoogleTranslator(source='en', target='uk').translate(ut_s)
